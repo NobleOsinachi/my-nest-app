b97a77a44ac9c1421b25a285d142caddfa6d8ec6
Noble Osinachi <nobleosinachi98@gmail.com>
2023-07-24 21:43:05 +0100

    Set up dynamic PORT

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Use process.env.PORT if available, or fallback to 3000
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+
+  // await app.listen(3000);
 }
 bootstrap();
